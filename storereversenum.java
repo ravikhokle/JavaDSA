fbe708038d11d82b7dc9622f1c3b4a8c560ff026
ravikhokle <ravikhokle1@gmail.com>
2026-08-06 15:24:30 +0530

    store reverse number

---
+++ storereversenum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class storereversenum{
+    public static void main(String args[]){
+        int num=456, lastDigit;
+        int i, rev=0;
+
+        while(num>0){
+            lastDigit = num%10; // return last digit  
+            num=num/10; // remove last digit
+            rev = (rev*10) + lastDigit; // add last digit to the reversed number
+            
+        }
+        System.out.print(rev);
+
+    }
+}
