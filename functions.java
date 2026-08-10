b3c1d4c605ed8229d9b80cfda011830bf3addab7
ravikhokle <ravikhokle1@gmail.com>
2026-08-10 18:50:30 +0530

    Prime Number Function

--- functions.java
+++ functions.java
@@ -14,25 +14,35 @@ public class functions{
         return fact;
     }
 
+    // nCr = n!/r!*(n-r)!
+    public static int combination(int n, int r){
+        int nfact = factorial(n);
+        int rfact = factorial(r);
+        int nmrfact = factorial(n-r);
+
+        int nCr = nfact / (rfact*nmrfact);
+
+        return nCr;
+    }
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
-            System.out.print("Enter first Num: ");
-            int a = sc.nextInt();
-            System.out.print("Enter second Num: ");
-            int b = sc.nextInt();
-            int ans = add(a,b);
+        //     System.out.print("Enter first Num: ");
+        //     int a = sc.nextInt();
+        //     System.out.print("Enter second Num: ");
+        //     int b = sc.nextInt();
+        //     int ans = add(a,b);
 
-            System.out.println("Sum: "+ans);
+        //     System.out.println("Sum: "+ans);
 
         
-        System.out.print("Enter number to calculate Factorial: ");
-        int n = sc.nextInt();
-        System.out.print("Factorial: "+ factorial(n));
+        // System.out.print("Enter number to calculate Factorial: ");
+        // int n = sc.nextInt();
+        // System.out.print("Factorial: "+ factorial(n));
+
+        System.out.print("Combination: "+ combination(5,2));
+
     }
-
-
-
-
 
 }
---
+++ primeNumFun.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class primeNumFun{
+
+    public static boolean checkPrime(int n){
+
+        if(n==2){
+            return true;
+        }
+        
+        for(int i=2; i<=n-1; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+}
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        boolean prime = checkPrime(n);
+
+        if(prime){
+            System.out.println("Number is Prime");
+        }else{
+            System.out.println("Number is Not Prime");
+        }
+
+    }
+}
