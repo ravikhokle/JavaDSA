eed9e5a204ba71effe3d4fc891423f7214568327
ravikhokle <ravikhokle1@gmail.com>
2026-08-10 20:12:58 +0530

    avg of three numbers

---
+++ avgFun.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class avgFun{
+    
+    public static float avgOfThreeNum(float a, float b, float c){
+        return (a+b+c)/3;
+    }
+
+    public static void main(String args[]){
+        System.out.println(avgOfThreeNum(2,3,5));
+    }
+}
--- primeNumFun.java
+++ primeNumFun.java
@@ -2,32 +2,56 @@ import java.util.*;
 
 public class primeNumFun{
 
-    public static boolean checkPrime(int n){
+//     public static boolean checkPrime(int n){
+//         if(n==2){
+//             return true;
+//         }
+//         for(int i=2; i<=n-1; i++){
+//             if(n%i==0){
+//                 return false;
+//             }
+//         }
+//         return true;
+// }
 
-        if(n==2){
-            return true;
-        }
-        
-        for(int i=2; i<=n-1; i++){
-            if(n%i==0){
-                return false;
-            }
-        }
+
+public static boolean checkPrime(int n){
+    if(n==2){
         return true;
+    }
+    for(int i=2; i<=Math.sqrt(n); i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
 }
+
+public static void primeInRange(int n){
+    for(int i=2; i<=n; i++){
+        if(checkPrime(i)){
+          System.out.print(" "+i);  
+        }
+    }
+}
+
 
     public static void main(String args[]){
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        int n = sc.nextInt();
-        boolean prime = checkPrime(n);
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter number: ");
+        // int n = sc.nextInt();
 
-        if(prime){
-            System.out.println("Number is Prime");
-        }else{
-            System.out.println("Number is Not Prime");
-        }
+
+        // boolean prime = checkPrime(n);
+        // if(prime){
+        //     System.out.println("Number is Prime");
+        // }else{
+        //     System.out.println("Number is Not Prime");
+        // }
+
+        primeInRange(20);
+
 
     }
 }
