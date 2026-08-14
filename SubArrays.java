238da78beb780437fa21d060678191ce9046f3c6
ravikhokle <ravikhokle1@gmail.com>
2026-08-14 19:11:21 +0530

    find sub arrays

---
+++ SubArrays.java
@@ -0,0 +1,19 @@
+public class SubArrays{
+
+    public static void printSubArrays(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i; j<arr.length; j++){
+                for(int k=i; k<=j; k++){
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        int numbers[] = {2,4,6,8,10};
+        printSubArrays(numbers);
+    }
+}
