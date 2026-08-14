3c36ba5ccd82e7dca1c8b1d17b4b1901b9b3448c
ravikhokle <ravikhokle1@gmail.com>
2026-08-14 18:36:29 +0530

    Array print pairs

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,13 +1,24 @@
 public class ReverseArray{
 
-    public static void reverse(int numbers[]){
-        int first=0, last=numbers.length-1;
-        while(first < last){
-            int temp=numbers[first];
-            numbers[first] = numbers[last];
-            numbers[last] = temp;
-            first++;
-            last--;
+    // public static void reverse(int numbers[]){
+    //     int first=0, last=numbers.length-1;
+    //     while(first < last){
+    //         int temp=numbers[first];
+    //         numbers[first] = numbers[last];
+    //         numbers[last] = temp;
+    //         first++;
+    //         last--;
+    //     }
+    // }
+
+    public static void reverseArray(int arr[]){
+        int p=0; int q=arr.length-1;
+        while(p<q){
+            int temp=arr[p];
+            arr[p]=arr[q];
+            arr[q]=temp;
+            p++;
+            q--;
         }
     }
 
@@ -18,9 +29,10 @@ public class ReverseArray{
         for(int i=0; i<numbers.length; i++){
             System.out.print(" "+ numbers[i]);
         }
-
+        
         System.out.println("\nAfter Reverse");
-        reverse(numbers);
+        reverseArray(numbers);
+        
         for(int i=0; i<numbers.length; i++){
             System.out.print(" "+ numbers[i]);
         }
