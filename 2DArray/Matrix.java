679348f65ae62e458bb2443cb5024d8d1d34e2f8
ravikhokle <ravikhokle1@gmail.com>
2026-08-24 22:49:59 +0530

    Matrix

---
+++ 2DArray/Matrix.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Matrix{
+
+public static void main(String args[]){
+
+    int matrix[][] = new int[3][3];
+    int m = matrix.length;
+    int n = matrix[0].length;
+    Scanner sc = new Scanner(System.in);
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            matrix[i][j] = sc.nextInt();
+        }
+    }
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            System.out.print(matrix[i][j] + " ");
+        }
+        System.out.println();
+    }
+
+}
+
+}
