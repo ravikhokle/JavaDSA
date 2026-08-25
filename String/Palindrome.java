2384a2c0f13a05704044463fa77317e29eabc6c4
ravikhokle <ravikhokle1@gmail.com>
2026-08-25 13:28:05 +0530

    Palindrome String

---
+++ String/Palindrome.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Palindrome{
+
+    public static boolean isPalindrome(String str){
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        String str = "madam";
+        System.out.print(isPalindrome(str));
+
+    }
+}
