97958f53c4596644de25768bbd523dc83ea05ab8
ravikhokle <ravikhokle1@gmail.com>
2026-08-30 22:50:21 +0530

    largest string

---
+++ String/LargestString.java
@@ -0,0 +1,17 @@
+public class LargestString {
+    public static void largestString(String[] str) {
+        String largest = str[0];
+        for (int i = 1; i < str.length; i++) {
+            if (largest.compareTo(str[i])<0) {
+                largest = str[i];
+            }
+        }
+        System.out.println("largest: "+largest);
+    }
+
+    public static void main(String args[]) {
+        String[] str={"apple","banana","mango"};
+
+        largestString(str);
+    }
+}
