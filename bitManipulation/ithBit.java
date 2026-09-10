4d1ba84023301efca04e42fd279a0b1ad5205cd5
ravikhokle <ravikhokle1@gmail.com>
2026-09-10 10:45:50 +0530

    Bit get, set, update

---
+++ bitManipulation/ithBit.java
@@ -0,0 +1,36 @@
+public class ithBit{
+
+    public static int getIthBit(int n, int i){
+        int bitMask = 1<<i;
+        if((n & bitMask) == 0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+    public static int setIthBit(int n, int i){
+        int bitMask = 1<<i;
+        return n | bitMask;
+    }
+
+    public static int clearIthBit(int n, int i){
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+    }
+
+    public static int UpdateIthBit(int n, int i,int newBit){
+        if(newBit == 0){
+            return clearIthBit(n,i);
+        }else{
+            return setIthBit(n,i);
+        }
+    }
+
+    public static void main(String args[]){
+        System.out.println(getIthBit(4,2));
+        System.out.println(setIthBit(10,2));
+        System.out.println(clearIthBit(10,1));
+        System.out.println(UpdateIthBit(10,2,1));
+    }
+}
