f04578c91a293dc710372c88ea1af2b994d50990
ravikhokle <ravikhokle1@gmail.com>
2026-09-12 19:48:55 +0530

    Bit Manipulation Clear Ith and Range of Bits

--- bitManipulation/ithBit.java
+++ bitManipulation/ithBit.java
@@ -27,10 +27,24 @@ public class ithBit{
         }
     }
 
+    public static int clearIBit(int n, int i){
+        int bitMask = (~0)<<i;
+        return n & bitMask;
+    }
+
+    public static int clearRangeOfBits(int n, int i, int j){
+        int a = ((~0)<<(j+1));
+        int b = (1<<i)-1;
+        int bitMask = a|b;
+        return n & bitMask;
+    }
+
     public static void main(String args[]){
         System.out.println(getIthBit(4,2));
         System.out.println(setIthBit(10,2));
         System.out.println(clearIthBit(10,1));
         System.out.println(UpdateIthBit(10,2,1));
+        System.out.println(clearIBit(15,2));
+        System.out.println(clearRangeOfBits(10,2,4));
     }
 }
